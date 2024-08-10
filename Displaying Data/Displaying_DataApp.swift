b66d3964c88efd70a658cbb4798d3ee32a681557
R. Kukuh <rkukuh@gmail.com>
2024-08-10 20:49:20 +0700

    Initial Commit

---
+++ Displaying Data/Displaying_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Displaying_DataApp.swift
+//  Displaying Data
+//
+//  Created by R. Kukuh on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Displaying_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
