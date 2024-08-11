94b1bb66e134ac8bf1787a2fe5321095d6ed7c4e
R. Kukuh <rkukuh@gmail.com>
2024-08-11 21:47:47 +0700

    Finish scroll directly to the row with ID 50

--- Displaying Data/ContentView.swift
+++ Displaying Data/ContentView.swift
@@ -9,36 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List {
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
+        ScrollViewReader { proxy in
+            VStack {
+                Button("Jump to #50") {
+                    proxy.scrollTo(50)
+                }
+                
+                List(0..<1000, id: \.self) { i in
+                    Text("Example \(i)")
+                        .id(i)
+                }
+            }
         }
     }
 }
