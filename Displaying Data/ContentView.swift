4695d68e6c8d469e70cf5d3b1c0fabbb3f9c4d18
R. Kukuh <rkukuh@gmail.com>
2024-08-11 23:18:37 +0700

    Finish adds two swipe actions per item shown in the list

--- Displaying Data/ContentView.swift
+++ Displaying Data/ContentView.swift
@@ -8,23 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    let friends = ["Antoine", "Bas", "Curt", "Dave", "Erica"]
+    
     var body: some View {
-        List {
-            Text("Pepperoni pizza")
-                .swipeActions {
-                    Button("Order") {
-                        print("Awesome!")
-                    }
-                    .tint(.green)
+        NavigationStack {
+            List {
+                ForEach(friends, id: \.self) { friend in
+                    Text(friend)
+                        .swipeActions(allowsFullSwipe: false) {
+                            Button {
+                                print("Muting conversation")
+                            } label: {
+                                Label("Mute", systemImage: "bell.slash.fill")
+                            }
+                            .tint(.indigo)
+                            
+                            Button(role: .destructive) {
+                                print("Deleting conversation")
+                            } label: {
+                                Label("Delete", systemImage: "trash.fill")
+                            }
+                        }
                 }
-            
-            Text("Pepperoni with pineapple")
-                .swipeActions {
-                    Button("Burn") {
-                        print("Right on!")
-                    }
-                    .tint(.red)
-                }
+            }
         }
     }
 }
