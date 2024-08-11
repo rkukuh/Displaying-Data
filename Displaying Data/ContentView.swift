11b3c520f24cbc1b116da23d1bfda6ca82f46dd8
R. Kukuh <rkukuh@gmail.com>
2024-08-11 21:06:05 +0700

    Finish allow the user to toggle editing mode

--- Displaying Data/ContentView.swift
+++ Displaying Data/ContentView.swift
@@ -8,38 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var users = ["Paul", "Taylor", "Adele", "Nicola"]
+    
     var body: some View {
-        List {
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
-            Text("Hello, static scrollable list")
+        NavigationStack {
+            List {
+                ForEach(users, id: \.self) { user in
+                    Text(user)
+                }
+                .onDelete(perform: delete)
+            }
+            .toolbar {
+                EditButton()
+            }
         }
+    }
+    
+    func delete(at offsets: IndexSet) {
+        users.remove(atOffsets: offsets)
     }
 }
 
