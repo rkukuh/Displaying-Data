2280b49b89fade20d818bffcb8b6a491fb125b25
R. Kukuh <rkukuh@gmail.com>
2024-08-13 01:40:17 +0700

    Finish: add scrollable functionality

--- Displaying Data/ContentView.swift
+++ Displaying Data/ContentView.swift
@@ -14,88 +14,90 @@ struct ContentView: View {
             GridItem(.flexible())
         ]
         
-        LazyVGrid(columns: columnLayout) {
-            VStack {
-                Image("hero-1")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(RoundedRectangle(cornerRadius: 18))
-                Text("Aamon")
+        ScrollView {
+            LazyVGrid(columns: columnLayout) {
+                VStack {
+                    Image("hero-1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 18))
+                    Text("Aamon")
+                }
+                
+                VStack {
+                    Image("hero-2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 18))
+                    Text("Akai")
+                }
+                
+                VStack {
+                    Image("hero-3")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 18))
+                    Text("Aldous")
+                }
+                
+                VStack {
+                    Image("hero-4")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 18))
+                    Text("Alice")
+                }
+                
+                VStack {
+                    Image("hero-5")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 18))
+                    Text("Alpha")
+                }
+                
+                VStack {
+                    Image("hero-6")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 18))
+                    Text("Alucard")
+                }
+                
+                VStack {
+                    Image("hero-7")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 18))
+                    Text("Angela")
+                }
+                
+                VStack {
+                    Image("hero-8")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 18))
+                    Text("Argus")
+                }
+                
+                VStack {
+                    Image("hero-9")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 18))
+                    Text("Arlot")
+                }
+                
+                VStack {
+                    Image("hero-10")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 18))
+                    Text("Atlas")
+                }
             }
-            
-            VStack {
-                Image("hero-2")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(RoundedRectangle(cornerRadius: 18))
-                Text("Akai")
-            }
-            
-            VStack {
-                Image("hero-3")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(RoundedRectangle(cornerRadius: 18))
-                Text("Aldous")
-            }
-            
-            VStack {
-                Image("hero-4")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(RoundedRectangle(cornerRadius: 18))
-                Text("Alice")
-            }
-            
-            VStack {
-                Image("hero-5")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(RoundedRectangle(cornerRadius: 18))
-                Text("Alpha")
-            }
-            
-            VStack {
-                Image("hero-6")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(RoundedRectangle(cornerRadius: 18))
-                Text("Alucard")
-            }
-            
-            VStack {
-                Image("hero-7")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(RoundedRectangle(cornerRadius: 18))
-                Text("Angela")
-            }
-            
-            VStack {
-                Image("hero-8")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(RoundedRectangle(cornerRadius: 18))
-                Text("Argus")
-            }
-            
-            VStack {
-                Image("hero-9")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(RoundedRectangle(cornerRadius: 18))
-                Text("Arlot")
-            }
-            
-            VStack {
-                Image("hero-10")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(RoundedRectangle(cornerRadius: 18))
-                Text("Atlas")
-            }
+            .padding(8)
         }
-        .padding(8)
     }
 }
 
