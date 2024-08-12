4c2ce7578b02ce190ab3da174f18294d6aa33b69
R. Kukuh <rkukuh@gmail.com>
2024-08-13 00:29:06 +0700

    Finish: use List component instead

--- Displaying Data/ContentView.swift
+++ Displaying Data/ContentView.swift
@@ -9,213 +9,167 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .top) {
-            Image("hero-1")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 80)
-                .clipShape(RoundedRectangle(cornerRadius: 18))
-            
-            VStack(alignment:.leading){
-                Text("Aamon")
-                    .font(.title3)
-                    .bold()
-                Text("Hero Class")
-                    .foregroundColor(.secondary)
+        List {
+            HStack(alignment: .top) {
+                Image("hero-1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80)
+                    .clipShape(RoundedRectangle(cornerRadius: 18))
+                
+                VStack(alignment:.leading){
+                    Text("Aamon")
+                        .font(.title3)
+                        .bold()
+                    Text("Hero Class")
+                        .foregroundColor(.secondary)
+                }
             }
             
-            Spacer()
-        }
-        .padding()
-        
-        Divider()
-        
-        HStack(alignment: .top) {
-            Image("hero-2")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 80)
-                .clipShape(RoundedRectangle(cornerRadius: 18))
-            
-            VStack(alignment:.leading){
-                Text("Akai")
-                    .font(.title3)
-                    .bold()
-                Text("Hero Class")
-                    .foregroundColor(.secondary)
+            HStack(alignment: .top) {
+                Image("hero-2")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80)
+                    .clipShape(RoundedRectangle(cornerRadius: 18))
+                
+                VStack(alignment:.leading){
+                    Text("Akai")
+                        .font(.title3)
+                        .bold()
+                    Text("Hero Class")
+                        .foregroundColor(.secondary)
+                }
             }
             
-            Spacer()
-        }
-        .padding()
-        
-        Divider()
-        
-        HStack(alignment: .top) {
-            Image("hero-3")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 80)
-                .clipShape(RoundedRectangle(cornerRadius: 18))
-            
-            VStack(alignment:.leading){
-                Text("Aldous")
-                    .font(.title3)
-                    .bold()
-                Text("Hero Class")
-                    .foregroundColor(.secondary)
+            HStack(alignment: .top) {
+                Image("hero-3")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80)
+                    .clipShape(RoundedRectangle(cornerRadius: 18))
+                
+                VStack(alignment:.leading){
+                    Text("Aldous")
+                        .font(.title3)
+                        .bold()
+                    Text("Hero Class")
+                        .foregroundColor(.secondary)
+                }
             }
             
-            Spacer()
-        }
-        .padding()
-        
-        Divider()
-        
-        HStack(alignment: .top) {
-            Image("hero-4")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 80)
-                .clipShape(RoundedRectangle(cornerRadius: 18))
-            
-            VStack(alignment:.leading){
-                Text("Alice")
-                    .font(.title3)
-                    .bold()
-                Text("Hero Class")
-                    .foregroundColor(.secondary)
+            HStack(alignment: .top) {
+                Image("hero-4")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80)
+                    .clipShape(RoundedRectangle(cornerRadius: 18))
+                
+                VStack(alignment:.leading){
+                    Text("Alice")
+                        .font(.title3)
+                        .bold()
+                    Text("Hero Class")
+                        .foregroundColor(.secondary)
+                }
             }
             
-            Spacer()
-        }
-        .padding()
-        
-        Divider()
-        
-        HStack(alignment: .top) {
-            Image("hero-5")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 80)
-                .clipShape(RoundedRectangle(cornerRadius: 18))
-            
-            VStack(alignment:.leading){
-                Text("Alpha")
-                    .font(.title3)
-                    .bold()
-                Text("Hero Class")
-                    .foregroundColor(.secondary)
+            HStack(alignment: .top) {
+                Image("hero-5")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80)
+                    .clipShape(RoundedRectangle(cornerRadius: 18))
+                
+                VStack(alignment:.leading){
+                    Text("Alpha")
+                        .font(.title3)
+                        .bold()
+                    Text("Hero Class")
+                        .foregroundColor(.secondary)
+                }
             }
             
-            Spacer()
-        }
-        .padding()
-        
-        Divider()
-        
-        HStack(alignment: .top) {
-            Image("hero-6")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 80)
-                .clipShape(RoundedRectangle(cornerRadius: 18))
-            
-            VStack(alignment:.leading){
-                Text("Alucard")
-                    .font(.title3)
-                    .bold()
-                Text("Hero Class")
-                    .foregroundColor(.secondary)
+            HStack(alignment: .top) {
+                Image("hero-6")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80)
+                    .clipShape(RoundedRectangle(cornerRadius: 18))
+                
+                VStack(alignment:.leading){
+                    Text("Alucard")
+                        .font(.title3)
+                        .bold()
+                    Text("Hero Class")
+                        .foregroundColor(.secondary)
+                }
             }
             
-            Spacer()
-        }
-        .padding()
-        
-        Divider()
-        
-        HStack(alignment: .top) {
-            Image("hero-7")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 80)
-                .clipShape(RoundedRectangle(cornerRadius: 18))
-            
-            VStack(alignment:.leading){
-                Text("Angela")
-                    .font(.title3)
-                    .bold()
-                Text("Hero Class")
-                    .foregroundColor(.secondary)
+            HStack(alignment: .top) {
+                Image("hero-7")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80)
+                    .clipShape(RoundedRectangle(cornerRadius: 18))
+                
+                VStack(alignment:.leading){
+                    Text("Angela")
+                        .font(.title3)
+                        .bold()
+                    Text("Hero Class")
+                        .foregroundColor(.secondary)
+                }
             }
             
-            Spacer()
-        }
-        .padding()
-        
-        Divider()
-        
-        HStack(alignment: .top) {
-            Image("hero-8")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 80)
-                .clipShape(RoundedRectangle(cornerRadius: 18))
-            
-            VStack(alignment:.leading){
-                Text("Argus")
-                    .font(.title3)
-                    .bold()
-                Text("Hero Class")
-                    .foregroundColor(.secondary)
+            HStack(alignment: .top) {
+                Image("hero-8")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80)
+                    .clipShape(RoundedRectangle(cornerRadius: 18))
+                
+                VStack(alignment:.leading){
+                    Text("Argus")
+                        .font(.title3)
+                        .bold()
+                    Text("Hero Class")
+                        .foregroundColor(.secondary)
+                }
             }
             
-            Spacer()
-        }
-        .padding()
-        
-        Divider()
-        
-        HStack(alignment: .top) {
-            Image("hero-9")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 80)
-                .clipShape(RoundedRectangle(cornerRadius: 18))
-            
-            VStack(alignment:.leading){
-                Text("Arlott")
-                    .font(.title3)
-                    .bold()
-                Text("Hero Class")
-                    .foregroundColor(.secondary)
+            HStack(alignment: .top) {
+                Image("hero-9")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80)
+                    .clipShape(RoundedRectangle(cornerRadius: 18))
+                
+                VStack(alignment:.leading){
+                    Text("Arlott")
+                        .font(.title3)
+                        .bold()
+                    Text("Hero Class")
+                        .foregroundColor(.secondary)
+                }
             }
             
-            Spacer()
-        }
-        .padding()
-        
-        Divider()
-        
-        HStack(alignment: .top) {
-            Image("hero-10")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 80)
-                .clipShape(RoundedRectangle(cornerRadius: 18))
-            
-            VStack(alignment:.leading){
-                Text("Atlas")
-                    .font(.title3)
-                    .bold()
-                Text("Hero Class")
-                    .foregroundColor(.secondary)
+            HStack(alignment: .top) {
+                Image("hero-10")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80)
+                    .clipShape(RoundedRectangle(cornerRadius: 18))
+                
+                VStack(alignment:.leading){
+                    Text("Atlas")
+                        .font(.title3)
+                        .bold()
+                    Text("Hero Class")
+                        .foregroundColor(.secondary)
+                }
             }
-            
-            Spacer()
         }
-        .padding()
     }
 }
 
